67b3e26a41c73df3c99e255a99c33ecf54f37fc5
realsubodh <subodhsingh3477@gmail.com>
2025-02-07 21:58:45 +0530

    nodemon

--- Week 3/assignment1.js
+++ Week 3/assignment1.js
@@ -3,5 +3,69 @@
 */
 
 const express = require("express")
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken")  // you are suppose to install jsonwebtoken along with express in the terminal
 const jwtPassword = "123456"
+
+const app = express()
+
+const ALL_USERS = [
+    {
+        username: "subodh@gmail.com",
+        passoword: "123",
+        name:"Subodh Singh"
+    },
+    {
+        username:"aadi@gmail.com",
+        passoword:"1234",
+        name:"Aadi"
+    },
+    {
+        username:"shivam@gmail.com",
+        passoword:"12345",
+        name:"Shivam Singh"
+    }
+]
+
+function userExists(username, password){
+    // hard todo - try to use the find function in js
+    const userExists = false;
+    for (let i = 0; i<ALL_USERS.length; i++){
+        if(ALL_USERS[i].username == username && ALL_USERS[i].password == password){
+            userExists = true;
+        }
+    }
+    return userExists;
+}
+
+app.post("/signin", function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(!userExists(username,password)){
+        return res.status(403).json({
+            msg: "user doesnot exist in our in memomory database",
+        })
+    }
+
+    var token = jwt.sign({username : username},"shhhh")
+    return res.json({
+        token,
+    })
+})
+
+app.get("/users", function(req,res){
+    const token = req.headers.authorization;
+    try{
+        const decoded = jwt.verify(token, jwtPassword)
+        const username = decoded.username;
+
+        // return a list of users other than this username
+    }
+    catch(err){
+        return res.status(403).jsom({
+            msg:"invalid token",
+        })
+    }
+})
+
+app.listen(3000)
