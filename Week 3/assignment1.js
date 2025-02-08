84d81359bca92f029ede02fd9c3c1a258f09750d
realsubodh <subodhsingh3477@gmail.com>
2025-02-09 02:22:19 +0530

    assignment completed

--- Week 3/assignment1.js
+++ Week 3/assignment1.js
@@ -7,28 +7,28 @@ const jwt = require("jsonwebtoken")  // you are suppose to install jsonwebtoken 
 const jwtPassword = "123456"
 
 const app = express()
-
+app.use(express.json()) // omp for the decoding the body otherwise the server will throw the error
 const ALL_USERS = [
     {
         username: "subodh@gmail.com",
-        passoword: "123",
+        password: "123",
         name:"Subodh Singh"
     },
     {
         username:"aadi@gmail.com",
-        passoword:"1234",
+        password:"1234",
         name:"Aadi"
     },
     {
         username:"shivam@gmail.com",
-        passoword:"12345",
+        password:"12345",
         name:"Shivam Singh"
     }
 ]
 
 function userExists(username, password){
     // hard todo - try to use the find function in js
-    const userExists = false;
+    let userExists = false;
     for (let i = 0; i<ALL_USERS.length; i++){
         if(ALL_USERS[i].username == username && ALL_USERS[i].password == password){
             userExists = true;
@@ -47,7 +47,7 @@ app.post("/signin", function(req,res){
         })
     }
 
-    var token = jwt.sign({username : username},"shhhh")
+    var token = jwt.sign({username : username},jwtPassword)
     return res.json({
         token,
     })
@@ -59,13 +59,28 @@ app.get("/users", function(req,res){
         const decoded = jwt.verify(token, jwtPassword)
         const username = decoded.username;
 
-        // return a list of users other than this username
+        // return a list of users other than his username
+        // that means user can see the other users rather than himself oly.
+        res.json({
+            users:ALL_USERS.filter(function(value){
+                if(value.username == username){
+                    return false
+                }
+                else{
+                    return true;
+                }
+            })
+        })
     }
     catch(err){
-        return res.status(403).jsom({
+        return res.status(403).json({
             msg:"invalid token",
         })
     }
 })
 
 app.listen(3000)
+
+
+// remember JWT is a kinda of library which provides us a lot of features
+// and common one which we use most is signin and verify.
