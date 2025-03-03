c56922544d3ab594468e2d48aa4bff51ab74f7c5
realsubodh <subodhsingh3477@gmail.com>
2025-03-03 21:59:22 +0530

    MongoDb assignment Completed

--- assignments-master/week-3/03-mongo/routes/admin.js
+++ assignments-master/week-3/03-mongo/routes/admin.js
@@ -9,7 +9,7 @@ router.post('/signup', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    // check if a user with this username already exists
+    // check if a admin with this username already exists
     await Admin.create({
         username:username,
         password:password
@@ -19,23 +19,31 @@ router.post('/signup', async (req, res) => {
         })
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
      const title = req.body.title;
      const description = req.body.description;
      const imageLink = req.body.imageLink;
      const price = req.body.price;
      // make sure to use zod for some sort of input validation
-     Course.create({
+     // you have to use async/await because you have to wait until the user creates the entry so that you can return the id.
+     const newCourse = await Course.create({
         title,
         description,
         imageLink,
         price
      })
+    res.json({
+        message:"course created successfully", courseId: newCourse._id
+    })
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    const response = await Course.find({})
+    res.json({
+        courses:response
+    })
 });
 
 module.exports = router;
