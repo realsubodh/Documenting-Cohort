8cee5be05b508d008503e1315cf7aadc5a5f7a78
realsubodh <subodhsingh3477@gmail.com>
2025-03-04 23:19:50 +0530

    Doing jwt assignment

--- assignments-master/week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ assignments-master/week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,28 @@
+const jwt = require("jsonwebtoken")
+const secret = require("../index")
+
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    // with this you get token like this bearer aabdbasbdsbadbsd
+    const words = token.split(" ")
+    // this will convert the token in the form of array like this [bearer,(adsfdssfsfsf)]
+    const jwtToken = words[1]
+    // with this you can access the real token string which is on index one
+
+    const decodedValue = jwt.verify(jwtToken,secret)
+    if(decodedValue.username){
+        next()
+    }
+    else{
+        res.status(403).json({
+            msg:"You are not authenticated!!"
+        })
+    }
+
 }
 
 module.exports = adminMiddleware;
--- assignments-master/week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ assignments-master/week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,27 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const {Admin} = require("../db")
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // check if a admin with this username already exists
+    await Admin.create({
+        username:username,
+        password:password
+    })
+        res.json({
+            message: 'Admin created successfully'
+        })
 });
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
