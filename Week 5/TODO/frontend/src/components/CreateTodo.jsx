ab4656e4c20bac4402f5df78681340a9055610d9
realsubodh <subodhsingh3477@gmail.com>
2025-03-12 20:05:53 +0530

    Completed the backend of the app

---
+++ Week 5/TODO/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+export function CreateTodo(){
+    return
+    <div>
+        <input type="text" name="" id="" placeholder="title"/> <br />
+        <input type="text" name="" id="" placeholder="description"/> <br />
+
+        <button>Add Todo</button>
+    </div>
+}
+
