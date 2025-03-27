540204729f61ebcd55c27dfda9557eee8e6ddefa
realsubodh <subodhsingh3477@gmail.com>
2025-03-27 20:30:14 +0530

    useNavigate()

--- Week 7/w7/src/App.jsx
+++ Week 7/w7/src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
+import { Dashboard } from "./components/Dashboard";
+import { Landing } from "./components/Landing";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-  
-    </>
-  )
+    <div>
+      <div>
+        <button onClick={()=>{
+          window.location.href="/"
+        }}
+        >Landing Page</button>
+        <button onClick={()=>{
+          window.location.href="/Dashboard"
+        }}>Dashboard Page</button>
+        
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
