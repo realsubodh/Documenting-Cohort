f1b99cd7491674b68497506f7f4d63ffc8f4586a
realsubodh <subodhsingh3477@gmail.com>
2025-03-29 03:06:18 +0530

    useContext API

--- Week 7/w7/src/App.jsx
+++ Week 7/w7/src/App.jsx
@@ -1,54 +1,48 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-import "./App.css";
-import { lazy, Suspense } from "react";
-const Dashboard = lazy(()=> import('./components/Dashboard'))
-const Landing  = lazy(()=> import('./components/Landing'))
+import { useContext, useState } from "react";
+import { CountContext } from "./context";
 
-function App() {
-  // for lazy loading we have to use the Suspense API and it is must be there
+
+function App(){
+  const [count,setCount] = useState(0)
+
+  // wrap anyone that wants to use the teleported value inside a provider
+  // context api == teleporter
   return (
     <div>
-      <div> 
-        {/* <button onClick={()=>{
-          window.location.href="/"
-        }}
-        >Landing Page</button>
-        <button onClick={()=>{
-          window.location.href="/Dashboard"
-        }}>Dashboard Page</button>
-        this one is not the feasible way because it reloads the whole web page.
-        */}
-      </div>
-      <BrowserRouter>
-        <Appbar />
-        <Routes>
-          <Route path="/dashboard" element={<Suspense fallback={"loading..."}><Dashboard /></Suspense>} />
-          <Route path="/" element={<Suspense fallback={"loading..."}><Landing /></Suspense>} />
-        </Routes>
-      </BrowserRouter>
+      <CountContext.Provider value ={count}>
+      <Count count={count} setCount={setCount}/>
+      </CountContext.Provider>
     </div>
-  );
+  )
 }
 
-function Appbar() {
-  const navigate = useNavigate();
-  return (
-    <div>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        Landing Page
-      </button>
-      <button
-        onClick={() => {
-          navigate("/dashboard");
-        }}
-      >
-        Dashboard Page
-      </button>
-    </div>
-  );
+function Count({count, setCount}){
+return <div>
+  <CountRenderer count={count}/>
+  <Buttons count={count} setCount={setCount}/>
+</div>
 }
-export default App;
+
+function CountRenderer(){
+  const count = useContext(CountContext)
+  return <div>
+    {count}
+  </div>
+}
+function Buttons({setCount}){
+  const count = useContext(CountContext)
+return <div>
+  <button onClick={()=>{
+     setCount(count + 1)
+  }}>
+    Increase
+  </button>
+
+  <button onClick={()=>{
+    setCount(count -1)
+  }}>
+    Descrease
+  </button>
+</div>
+}
+export default App
