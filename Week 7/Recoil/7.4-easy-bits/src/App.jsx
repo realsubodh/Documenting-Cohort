875680b212350cca772ccb2d55d9fdd928b6801e
realsubodh <subodhsingh3477@gmail.com>
2025-04-19 00:44:20 +0530

    completed 7.4-easy-bits

--- Week 7/Recoil/7.4-easy-bits/src/App.jsx
+++ Week 7/Recoil/7.4-easy-bits/src/App.jsx
@@ -1,20 +1,35 @@
 
 //RecoilRoot
 
-import { useState } from "react"
+import { useRecoilValue, RecoilRoot, useRecoilState } from "recoil"
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totatlNotificationSelector } from "./atoms"
+
 
 function App() {
-  const [count,setCount]= useState(0)
+  // wrapping everything under the recoil root is must
+ return <RecoilRoot> 
+  <MainApp/>
+ </RecoilRoot>
+}
+function MainApp(){
+  const networkNotificationCount = useRecoilValue(networkAtom)
+  const jobsAtomCount = useRecoilValue(jobsAtom)
+  const notificationAtomCount = useRecoilValue(notificationsAtom)
+  const [messageAtomCount, setMessagingCount] = useRecoilState(messagingAtom)
+  const totalNotificationCount = useRecoilValue(totatlNotificationSelector)
+  // upperOne is same as useState
   return(
     <>
       <button>Home</button>
 
-      <button>My Network ()</button>
-      <button>Jobs ()</button>
-      <button>Messages ()</button>
-      <button>Notifications ()</button>
+      <button>My Network({networkNotificationCount >= 100 ? "99+" : networkNotificationCount })</button>
+      <button>Jobs {jobsAtomCount}</button>
+      <button>Messages {messageAtomCount}</button>
+      <button>Notifications {notificationAtomCount}</button>
 
-      <button>Me</button>
+      <button onClick={()=>{
+        setMessagingCount(messageAtomCount + 1)
+      }}>Me ({totalNotificationCount})</button>
     </>
   )
 }
--- Week 7/Recoil/7.4-easy-bits/src/atoms.js
+++ Week 7/Recoil/7.4-easy-bits/src/atoms.js
@@ -1,21 +1,36 @@
-import {atom} from "recoil";
+import {atom, selector} from "recoil";
 
+// L1: never repeat the key in the different atoms layer
 export const networkAtom = atom({
     key: "networkAtom",
     default: 102
 })
 
 export const jobsAtom = atom({
-    key: "networkAtom",
+    key: "jobsAtom",
     default: 0
 })
 
 export const notificationsAtom = atom({
-    key: "networkAtom",
+    key: "notificationAtom",
     default: 12
 })
 
 export const messagingAtom = atom({
-    key: "networkAtom",
+    key: "messagingAtom",
     default: 0
+})
+
+// showing total notifications toasts on a single pane
+// using selector instead of useMemo()
+export const totatlNotificationSelector = selector({
+    key: "totalNotificationSelector",
+    get: ({get})=> {
+        const networkAtomCount = get(networkAtom)
+        const jobsAtomCount = get(jobsAtom)
+        const notificationAtomCount = get(notificationsAtom)
+        const messagingAtomCount = get(messagingAtom)
+
+        return networkAtomCount + jobsAtomCount + notificationAtomCount + messagingAtomCount
+    }
 })
