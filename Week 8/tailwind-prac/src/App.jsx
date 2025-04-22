51fe2f893deb58c9b430372dd4657517a2648784
realsubodh <subodhsingh3477@gmail.com>
2025-04-22 15:11:45 +0530

    completed TailwindCSS

--- Week 8/tailwind-prac/src/App.jsx
+++ Week 8/tailwind-prac/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 
 import './App.css'
+import { RevenueCard } from '../components/RevenueCard'
 
 function App() {
 
   return (
     <>
-
+      <div className='grid grid-cols-3'>
+      <RevenueCard title={"Amount pending"} amount = {"92,330.20"} orderCount={13} />
+      </div>
     </>
   )
 }
