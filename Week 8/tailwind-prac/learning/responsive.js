4682eb05af57855305fd9ad546f62fc9efd59ca9
realsubodh <subodhsingh3477@gmail.com>
2025-04-22 14:11:37 +0530

    responsiveness in tailwind

---
+++ Week 8/tailwind-prac/learning/responsive.js
@@ -0,0 +1,9 @@
+    {/* Responsiveness in the tailwind css */}
+    <div className='bg-red-500 sm:bg-blue-500'>Checking Responsiveness</div>
+
+    {/* Making grid responsive */}
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+      <div className='bg-blue-100 text-orange-700'>layout one</div>
+      <div className='bg-pink-100'>layout two</div>
+      <div className='bg-orange-100'>layout three</div>
+    </div>
