038be6158769cac1fb7930d6b7faf3dc5dbb4a37
realsubodh <subodhsingh3477@gmail.com>
2025-04-27 23:54:32 +0530

    backend

--- projects/paytm/backend/routes/user.js
+++ projects/paytm/backend/routes/user.js
@@ -1,7 +1,32 @@
 const express = require("express")
 const router = express.Router()
+const zod = require("zod")
+const { User } = require("../db")
 
+//signup route | zod validation
+const signupSchema = zod.object({
+    username: zod.string(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
+})
+router.post("/signup",(req,res) => {
+    const body = req.body
+    const {success} = signupSchema.safeParse(req.body)
+    if(!success){
+        return res.json({
+            message: "email already taken/incorrect inputs"
+        })
+    }
 
-router.post("/signup")
+    const user = User.findOne({
+        username: body.username
+    })
+    if(user.id){
+        return res.json({
+            message: "email already taken/ incorrect inputs"
+        })
+    }
+})
 
 module.exports = router
