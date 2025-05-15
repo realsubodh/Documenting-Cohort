32ec6afe7608941a2f54a7cfc04fff83116dcb95
realsubodh <subodhsingh3477@gmail.com>
2025-05-15 11:21:27 +0530

    Signup/Signin Page

---
+++ projects/paytm/frontend/src/pages/Signup.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { Heading } from "../components/Header"
+import { Subheading } from "../components/Subheading"
+import { InputBox } from "../components/InputBox"
+import { Button } from "../components/Button"
+import { BottomWarning } from "../components/BottomWarning"
+
+export const Signup =()=>{
+    return <div className="min-h-screen flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center rounded-t-lg w-[450px] h-[600px] bg-white shadow-[0_20px_50px_black] ">
+            <div className="rounded-lg w-full font-sans text-center p-2 h-max ">
+            <Heading label={"Sign Up"}/>
+            <Subheading label={"Enter your information to create your account"}/>
+            <InputBox onChange={e=>{}} placeholder="John" label={"First Name"}/>
+            <InputBox onChange={e=>{}} placeholder="Doe" label={"Last Name"}/>
+            <InputBox onChange={e=>{}} placeholder="subodhsingh@gmail.com" label={"Email Id"}/>
+            <InputBox onChange={e=>{}} placeholder="hkirat123" label={"Password"}/>
+            <Button onClick={async()=>{}} label={"Sign up"}/>
+            </div>
+            <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"}/>
+        </div>
+    </div>
+    
+}
