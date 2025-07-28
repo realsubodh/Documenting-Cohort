2a1ed2f570a03a17c33ce1800e593dd1d35b54cb
realsubodh <subodhsingh3477@gmail.com>
2025-07-28 09:26:08 +0530

    Typescript with React

---
+++ Week 9/ts-react/src/Learning/ReactTs.tsx
@@ -0,0 +1,21 @@
+function App() {
+    return(
+        <>
+        <Todo title='go yo the gym' description='heeyyyy go to the gym' done={false}/>
+        </>
+    )
+    }
+    
+    //  giving types to the react component
+    
+    interface TodoProp{
+        title:string,
+        description: string,
+        done: boolean
+    }
+    function Todo(props: TodoProp){
+        return <div>
+            <h1>{props.title}</h1>
+            <h2>{props.description}</h2>
+        </div>
+    }
